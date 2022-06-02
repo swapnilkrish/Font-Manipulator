6f4113d81eafa3089b7a1dbd6ee3668bdc87448d
swapnilkrish <86659449+swapnilkrish@users.noreply.github.com>
2022-06-02 12:37:07 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,26 @@
+difference = 0;
+rightWristX = 0;
+leftWristX = 0;
+
+function setup() {
+    video = CreateCapture(VIDEO)
+    video.size(550, 500);
+
+    canvas = createCanvas(550, 550);
+    canvas.position(560, 150);
+
+    poseNet = ml5.poseNet(video, modelLoaded);
+    poseNet.on('PoseNet Is Initialized!');
+}
+
+function gotPose(results) {
+    if (results.length > 0) {
+        console.log(results);
+
+        leftWristX = results[0].pose.leftWrist.x;
+        rightWristX = results[0].pose.rightWrist.x;
+        difference = floor(leftWristX - rightWristX);
+
+        console.log("leftWristX = " + leftWristX + " rightWristX = " + rightWristX + " difference = " + difference)
+    }
+}
