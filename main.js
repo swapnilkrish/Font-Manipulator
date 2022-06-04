a8acf03385423dc2fd210fa65408e9b0167aeebd
swapnilkrish <86659449+swapnilkrish@users.noreply.github.com>
2022-06-04 15:38:00 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -3,17 +3,17 @@ rightWristX = 0;
 leftWristX = 0;
 
 function setup() {
-    video = CreateCapture(VIDEO)
+    video = createCapture(VIDEO);
     video.size(550, 500);
 
     canvas = createCanvas(550, 550);
     canvas.position(560, 150);
 
     poseNet = ml5.poseNet(video, modelLoaded);
-    poseNet.on('PoseNet Is Initialized!');
+    poseNet.on('pose', gotPoses);
 }
 
-function gotPose(results) {
+function gotPoses(results) {
     if (results.length > 0) {
         console.log(results);
 
@@ -21,6 +21,19 @@ function gotPose(results) {
         rightWristX = results[0].pose.rightWrist.x;
         difference = floor(leftWristX - rightWristX);
 
-        console.log("leftWristX = " + leftWristX + " rightWristX = " + rightWristX + " difference = " + difference)
+        console.log("leftWristX = " + leftWristX + " rightWristX = " + rightWristX + " difference = " + difference);
     }
+}
+
+function draw() {
+    background('#6C91C2');
+
+    document.getElementById("font_size").innerHTML = "Font size of the text will be = " + difference + "px";
+    textSize(difference);
+    fill('#FFE787');
+    text("Krishna", 50, 400);
+}
+
+function modelLoaded() {
+    console.log("The model is loaded");
 }
